ab8911d445fd995e6ee392cddf28b754f8ccf734
Cristian Dago <cris.gallardos@gmail.com>
2025-04-17 05:19:52 -0400

    Cambio de ruta test

--- test/app.test.js
+++ test/app.test.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
 const express = require('express');
-const app = require('./app');
+const app = require('../app');
 
 // Prueba para GET /tasks
 describe('GET /tasks', () => {
